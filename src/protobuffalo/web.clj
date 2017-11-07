835f5032b960ebfe7be3ad9e68768761d96406c0
William Rudenmalm <me@whn.se>
2017-11-07 23:15:28 +0100

    add support for forms

--- src/protobuffalo/web.clj
+++ src/protobuffalo/web.clj
@@ -5,7 +5,11 @@
             [protobuffalo.loader :refer [encode-proto decode-proto]]
             [clojure.java.io :as io]
             [ring.middleware.stacktrace :refer [wrap-stacktrace-web]]
+            [ring.middleware.params :refer [wrap-params]]
             [clojure.edn :as edn]))
+
+(defn pp-str [x]
+  (with-out-str (clojure.pprint/pprint x)))
 
 (defn- file->bytes [stream]
   (with-open [xout (java.io.ByteArrayOutputStream.)]
@@ -15,14 +19,32 @@
 (defn- stream->str [stream]
   (String. (file->bytes stream)))
 
+(def buffalo "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo")
+
+(defn plain-text [text]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body text})
+
 (defn- mk-routes [loader]
   (routes
-   (GET "/healthz" [] "top kek")
-   (GET "/ready" [] "top kek")
-   (GET "/echo/:foo" request (str request))
+   (GET "/" []
+        {:status 200
+         :headers {}
+         :body (io/file "public/index.html")})
+   (GET "/healthz" [] buffalo)
+   (GET "/ready" [] buffalo)
+   (POST "/encode-form" [cls edn]
+         (if (seq cls)
+           (plain-text (String. (encode-proto loader cls (edn/read-string edn))))
+           "no class!"))
+   (POST "/decode-form" [cls protobuf]
+         (if (seq cls)
+           (plain-text (pp-str (decode-proto loader cls protobuf)))
+           "no class!"))
    (POST "/decode/:cls" {body :body {cls :cls} :route-params} [cls body]
          (if (seq cls)
-           (decode-proto loader cls (file->bytes body))
+           (pp-str (decode-proto loader cls (file->bytes body)))
            "no class!"))
    (POST "/encode/:cls" {body :body {cls :cls} :route-params}
          (if (seq cls)
@@ -34,6 +56,7 @@
   (start [this]
     (assoc this :handler
            (-> (mk-routes loader)
+               wrap-params
                wrap-stacktrace-web)))
   (stop [this]
     (assoc this :app nil)))
